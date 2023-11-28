7f97c51d0769494ec1249fa63d8de4f17409da7a
ayatakesi <ayanokoji.takesi@gmail.com>
2023-11-29 06:15:38 +0900

    define cp rule in Makefile.in instead of ndk-build-static-library.mk

--- cross/ndk-build/ndk-build-static-library.mk
+++ cross/ndk-build/ndk-build-static-library.mk
@@ -140,8 +140,3 @@ $(foreach source,$(NEON_SOURCE_FILES),$(eval $(call single-neon-target,$(source)
 # Now define the rule to build the library.
 $(LOCAL_MODULE_FILENAME): $(ALL_OBJECT_FILES$(LOCAL_MODULE))
 	$(NDK_BUILD_AR) r $@ $^
-
-# Add rule to copy generated library to the module's directory.
-$(LOCAL_PATH)/$(LOCAL_MODULE_FILENAME) : $(LOCAL_MODULE_FILENAME)
-	$(warning ayatakesi-debug-ndk-build-static-library $(LOCAL_PATH)/$(LOCAL_MODULE_FILENAME) : $(LOCAL_MODULE_FILENAME))
-	cp -pf $(LOCAL_MODULE_FILENAME) $(LOCAL_PATH)/$(LOCAL_MODULE_FILENAME)
