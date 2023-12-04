847933de83f9b9d942c06e9aad4d8e71cfe1de0b
ayatakesi <ayanokoji.takesi@gmail.com>
2023-12-04 21:13:48 +0900

    use = instead of := to process recursively

--- cross/ndk-build/ndk-build-shared-library.mk
+++ cross/ndk-build/ndk-build-shared-library.mk
@@ -163,7 +163,7 @@ $(foreach source,$(NEON_SOURCE_FILES),$(eval $(call single-neon-target,$(source)
 # which they depend, and also any shared libraries they depend on.
 
 define define-module-rule
-NDK_LOCAL_SO_NAMES_$(LOCAL_MODULE) := $(abspath $(NDK_LOCAL_SO_NAMES_$(LOCAL_MODULE)))
+NDK_LOCAL_SO_NAMES_$(LOCAL_MODULE) = $(abspath $(NDK_LOCAL_SO_NAMES_$(LOCAL_MODULE)))
 $(warning ayatakesi-debug: $(LOCAL_MODULE_FILENAME): $(ALL_OBJECT_FILES$(LOCAL_MODULE)) $(NDK_LOCAL_A_NAMES_$(LOCAL_MODULE)) $(NDK_WHOLE_A_NAMES_$(LOCAL_MODULE)) $(NDK_LOCAL_SO_NAMES_$(LOCAL_MODULE)))
 $(LOCAL_MODULE_FILENAME): $(ALL_OBJECT_FILES$(LOCAL_MODULE)) $(NDK_LOCAL_A_NAMES_$(LOCAL_MODULE)) $(NDK_WHOLE_A_NAMES_$(LOCAL_MODULE)) $(NDK_LOCAL_SO_NAMES_$(LOCAL_MODULE))
 	$(NDK_BUILD_CC) $(1) $(2) -o $$@ -shared $(NDK_LDFLAGS_$(LOCAL_MODULE)) $(NDK_SO_EXTRA_FLAGS_$(LOCAL_MODULE)) $(NDK_SO_DEFAULT_LDFLAGS) $(foreach so,$(NDK_LOCAL_SO_NAMES_$(LOCAL_MODULE)),-L $(abspath $(CURDIR)) -l:$(so))
