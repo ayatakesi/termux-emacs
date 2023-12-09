6a496a2db1b0713de72d2a2e3a9d8f1b78f913c3
ayatakesi <ayanokoji.takesi@gmail.com>
2023-12-09 20:26:33 +0900

    add warning, again

--- cross/ndk-build/ndk-build-shared-library.mk
+++ cross/ndk-build/ndk-build-shared-library.mk
@@ -130,6 +130,8 @@ endif
 _pos = $(if $(findstring $1,$2),$(call _pos,$1,$(wordlist 2,$(words $2),$2),x $3),$3)
 pos = $(words $(call _pos,$1,$2))
 ifeq ($(findstring $(LOCAL_MODULE),$(NDK_BUILD_ANDROID_MK_MODULE)),$(LOCAL_MODULE))
+
+$(warning ayatakesi-debug: pos,$(LOCAL_MODULE),$(NDK_BUILD_ANDROID_MK_MODULE),$(NDK_BUILD_ANDROID_MK))
 LOCAL_MODULE_ANDROID_MK = $(word $(call pos,$(LOCAL_MODULE),$(NDK_BUILD_ANDROID_MK_MODULE)),$(NDK_BUILD_ANDROID_MK))
 endif
 
